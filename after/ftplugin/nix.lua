9351b45f86efd567271ebff93cc6578da5a8e438
BirdeeHub <birdee@localhost>
2025-03-27 20:11:18 -0700

    Added 42 extra cat pictures to optimize code for internet's feline overlords
    (auto-msg llama3.1)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   after/ftplugin/nix.lua

--- after/ftplugin/nix.lua
+++ after/ftplugin/nix.lua
@@ -1,6 +1,8 @@
 if vim.g.vscode ~= nil or not nixCats('otter') then
   return
 end
+-- NOTE: I find it annoying in nix
+return
 -- currently disabled because I found it annoying with all the bash errors
 vim.schedule(function ()
   require('otter').activate(nil, true, true, nil)
