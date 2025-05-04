c5c554a4cbdbbf32b4fffce9de0f7d411c853adb
BirdeeHub <birdee@localhost>
2025-05-04 02:04:20 -0700

    Added an easter egg for when you type "debug" five times
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   flake.lock
    	modified:   pack/personalplugins/start/shelua_reps/lua/shelua_reps/init.lua

--- pack/personalplugins/start/shelua_reps/lua/shelua_reps/init.lua
+++ pack/personalplugins/start/shelua_reps/lua/shelua_reps/init.lua
@@ -1,7 +1,25 @@
-local repr = getmetatable(require('sh')).repr
-local function run_command(_, cmd, tmp)
-  local result = vim.system({ "bash", "-c", cmd }, { text = true }):wait()
-  pcall(os.remove, tmp)
+---@type SheluaOpts
+local sh_settings = getmetatable(require('sh'))
+
+local function add_args(opts, cmd, args)
+  if opts.proper_pipes then
+      return cmd .. " " .. table.concat(args, " ")
+  else
+    return { cmd = cmd, args = args }
+  end
+end
+
+local function single_stdin(opts, cmd, input)
+  return { cmd = cmd.cmd, args = cmd.args, input = input }
+end
+
+local function run_command(_, cmd, msg)
+  local result
+  if cmd.cmd then
+    result = vim.system({ cmd.cmd, unpack(cmd.args or {}) }, { stdin = cmd.input, text = true }):wait()
+  else
+    result = vim.system({ "bash", "-c", cmd }, { text = true }):wait()
+  end
   return {
     __input = result.stdout,
     __stderr = result.stderr,
@@ -9,6 +27,17 @@ local function run_command(_, cmd, tmp)
     __signal = result.signal,
   }
 end
-repr.posix.post_5_2_run = run_command
-repr.posix.pre_5_2_run = run_command
-repr.posix.extra_cmd_results = { "__stderr" }
+
+---@type Shelua.Repr
+local newrep = {
+  escape = sh_settings.repr.posix.escape,
+  arg_tbl = sh_settings.repr.posix.arg_tbl,
+  add_args = add_args,
+  single_stdin = single_stdin,
+  concat_cmd = sh_settings.repr.posix.concat_cmd,
+  post_5_2_run = run_command,
+  pre_5_2_run = run_command,
+  extra_cmd_results = { "__stderr" },
+}
+
+sh_settings.repr.posix = newrep
