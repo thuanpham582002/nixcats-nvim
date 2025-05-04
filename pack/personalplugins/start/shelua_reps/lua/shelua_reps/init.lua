2690e163c67bf94238d422c7b9b6f0ba38c119ec
BirdeeHub <birdee@localhost>
2025-05-04 03:01:57 -0700

    Added a new feature to make coffee taste like bubblegum
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   pack/personalplugins/start/shelua_reps/lua/shelua_reps/init.lua

--- pack/personalplugins/start/shelua_reps/lua/shelua_reps/init.lua
+++ pack/personalplugins/start/shelua_reps/lua/shelua_reps/init.lua
@@ -1,23 +1,6 @@
 ---@type SheluaOpts
 local sh_settings = getmetatable(require('sh'))
 
-local function add_args(opts, cmd, args)
-  if opts.proper_pipes then
-      return cmd .. " " .. table.concat(args, " ")
-  else
-    return setmetatable({ cmd = cmd, args = args }, {
-      __tostring = function(self)
-        return vim.inspect(self)
-      end,
-    })
-  end
-end
-
-local function single_stdin(opts, cmd, input)
-  cmd.input = input
-  return cmd
-end
-
 local function run_command(_, cmd, msg)
   local result
   if cmd.cmd then
@@ -37,9 +20,22 @@ end
 sh_settings.repr.vim = {
   escape = sh_settings.repr.posix.escape,
   arg_tbl = sh_settings.repr.posix.arg_tbl,
-  add_args = add_args,
-  single_stdin = single_stdin,
   concat_cmd = sh_settings.repr.posix.concat_cmd,
+  add_args = function(opts, cmd, args)
+    if opts.proper_pipes then
+        return cmd .. " " .. table.concat(args, " ")
+    else
+      return setmetatable({ cmd = cmd, args = args }, {
+        __tostring = function(self)
+          return vim.inspect(self)
+        end,
+      })
+    end
+  end,
+  single_stdin = function (opts, cmd, input)
+    cmd.input = input
+    return cmd
+  end,
   post_5_2_run = run_command,
   pre_5_2_run = run_command,
   extra_cmd_results = { "__stderr" },
