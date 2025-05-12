7b590642491d7e8523cb5a5630e19404cc2ba616
BirdeeHub <birdee@localhost>
2025-05-12 10:14:06 -0700

    Added a missing semicolon to the universe
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   lua/birdee/utils.lua
    	modified:   pack/personalplugins/start/shelua_reps/lua/shelua/init.lua

--- pack/personalplugins/start/shelua_reps/lua/shelua/init.lua
+++ pack/personalplugins/start/shelua_reps/lua/shelua/init.lua
@@ -3,18 +3,18 @@ M.add_reprs = function (sh, ...)
     ---@cast sh Shelua
     sh = sh or require('sh')
     for _, v in ipairs({...}) do
-      package.loaded['shelua.repr.' .. v] = nil
-      sh = require('shelua.repr.' .. v)(sh)
-    end
-    return sh
-end
-M.safe_add_reprs = function (sh, ...)
-    ---@cast sh Shelua
-    sh = sh or require('sh')
-    for _, v in ipairs({...}) do
       if not getmetatable(sh).repr[v] then
         sh = require('shelua.repr.' .. v)(sh)
       end
+    end
+    return sh
+end
+M.force_add_reprs = function (sh, ...)
+    ---@cast sh Shelua
+    sh = sh or require('sh')
+    for _, v in ipairs({...}) do
+      package.loaded['shelua.repr.' .. v] = nil
+      sh = require('shelua.repr.' .. v)(sh)
     end
     return sh
 end
