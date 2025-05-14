9cdf470d88e48388bccc106c334b1823ef5ac2d0
BirdeeHub <birdee@localhost>
2025-05-13 21:06:23 -0700

    Fixed the time loop on my laptop
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   pack/personalplugins/start/shelua_reps/lua/shelua/repr/posix_plus/init.lua

---
+++ pack/personalplugins/start/shelua_reps/lua/shelua/repr/posix_plus/init.lua
@@ -0,0 +1,98 @@
+---@param sh Shelua
+return function(sh)
+  ---@type Shelua.Opts
+  local sh_settings = getmetatable(sh)
+  local escapeShellArg = sh_settings.repr.posix.escape
+  local sherun = require('shelua.system').run
+
+  ---@param t table
+  ---@param default any
+  ---@vararg any
+  ---@return any
+  local function tbl_get(t, default, ...)
+      if #{ ... } == 0 then return default end
+      for _, key in ipairs({ ... }) do
+          if type(t) ~= "table" then return default end
+          t = t[key]
+      end
+      return t or default
+  end
+
+  ---@param opts Shelua.Opts
+  ---@param attr string
+  ---@return function
+  local get_repr_fn = function(opts, attr)
+      return tbl_get(opts, tbl_get(opts, function()
+          error("Shelua Repr Error: " ..
+              tostring(attr) .. " function required for shell: " .. tostring(opts.shell or "posix"))
+      end, "repr", "posix", attr), "repr", opts.shell or "posix", attr)
+  end
+
+  ---@type Shelua.Repr
+  sh_settings.repr.posix_plus = {
+    escape = escapeShellArg,
+    arg_tbl = function(opts, k, a)
+      k = (#k > 1 and '--' or '-') .. k
+      if type(a) == 'boolean' and a then return k end
+      if type(a) == 'string' then return { k, escapeShellArg(a) } end
+      if type(a) == 'number' then return { k, tostring(a) } end
+      return nil
+    end,
+    add_args = function(opts, cmd, args)
+      return cmd .. " " .. table.concat(args, ' ')
+    end,
+    extra_cmd_results = { "__env", "__stderr", "__cwd" },
+  }
+  -- TODO: allow AND, OR, CD, __cwd, and __env.
+  function sh_settings.repr.posix_plus.concat_cmd(opts, cmd, input)
+    if #input == 1 then
+        local v = input[1]
+        if v.s then
+            return 'printf "%s" ' .. get_repr_fn(opts, "escape")(v.s, opts) .. " | " .. cmd
+        else
+            return v.c .. " | " .. cmd
+        end
+    elseif #input > 1 then
+        for i = 1, #input do
+            local v = input[i]
+            if v.s then
+                input[i] = 'printf "%s" ' .. get_repr_fn(opts, "escape")(v.s, opts)
+            elseif v.c then
+                ---@diagnostic disable-next-line: assign-type-mismatch
+                input[i] = v.c
+            end
+        end
+        return "{ " .. table.concat(input, " ; ") .. " ; } | " .. cmd
+    else
+        return cmd
+    end
+  end
+  -- TODO: allow AND, OR, CD, __cwd, and __env.
+  function sh_settings.repr.posix_plus.single_stdin(opts, cmd, inputs, codes)
+    local tmp
+    if inputs then
+        tmp = os.tmpname()
+        local f = io.open(tmp, 'w')
+        if f then
+            f:write(table.concat(inputs))
+            f:close()
+            cmd = cmd .. ' <' .. tmp
+        end
+    end
+    return cmd, { tmp = tmp }
+  end
+
+  local function run_command(opts, cmd, msg)
+    local result = sherun({ "bash" }, { stdin = cmd, text = true }):wait()
+    pcall(os.remove, (msg or {}).tmp)
+    return {
+      __input = result.stdout,
+      __stderr = result.stderr,
+      __exitcode = result.code,
+      __signal = result.signal,
+    }
+  end
+  sh_settings.repr.posix_plus.post_5_2_run = run_command
+  sh_settings.repr.posix_plus.pre_5_2_run = run_command
+  return sh
+end
