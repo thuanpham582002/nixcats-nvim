1861842499e5228d3f1e9f86b0a3d8c45100aea8
BirdeeHub <birdee@localhost>
2025-05-25 10:49:24 -0700

    Reorganized the spaghetti code into a more readable lasagna structure
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   lua/fennel-init.lua

--- lua/fennel-init.lua
+++ lua/fennel-init.lua
@@ -1,24 +1,24 @@
 -- Source: https://neovim.discourse.group/t/how-to-use-fennel-in-runtime-scripts-without-compiling-to-lua/147
+local sep = package.config:sub(1, 1)
+local fennel_path = nixCats.configDir .. sep .. "fnl" .. sep
+local form = "%s?.fnl;%s?" .. sep .. "init.fnl;%s"
 local function _fennel_runtime_searcher(name)
-    local sep = package.config:sub(1, 1)
     local basename = name:gsub('%.', sep)
-    local paths = { "fnl" ..sep..basename..".fnl", "fnl"..sep..basename..sep.."init.fnl", }
+    local paths = { basename..".fnl", basename..sep.."init.fnl", }
+    for i, v in ipairs(paths) do paths[i] = fennel_path .. v end
     for _, path in ipairs(paths) do
-        local found = vim.api.nvim_get_runtime_file(path, false)
-        if #found > 0 then
+        if vim.fn.filereadable(path) == 1 then
             local ok, fennel = pcall(require, 'fennel')
-            if not ok then return end
-            _G.fennel = fennel
-            local fennel_path = nixCats.configDir .. sep .. "fnl" .. sep
-            local form = "%s?.fnl;%s?" .. sep .. "init.fnl;%s"
-            fennel.path = form:format(fennel_path, fennel_path, fennel.path)
-            fennel["macro-path"] = form:format(fennel_path, fennel_path, fennel["macro-path"])
             local loaders = package.loaders or package.searchers
             for i = #loaders, 1, -1 do
                 if loaders[i] == _fennel_runtime_searcher then
                     table.remove(loaders, i)
                 end
             end
+            if not ok then return end
+            _G.fennel = fennel
+            fennel.path = form:format(fennel_path, fennel_path, fennel.path)
+            fennel["macro-path"] = form:format(fennel_path, fennel_path, fennel["macro-path"])
             fennel.install()
             return function()
                 package.loaded[name] = nil
