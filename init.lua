30b9269165ba4cdddd778078099731f6e19b7339
BirdeeHub <birdee@localhost>
2025-05-25 06:10:27 -0700

    Added a feature to make coffee taste like beer
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   init.lua
    	modified:   lua/birdee/init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,22 @@
+string.relpath = function(str, sub, n)
+    local result = {}
+    n = type(sub) == "string" and n or sub
+    if type(n) == "number" and n > 0 then
+        for match in (str .. "."):gmatch("(.-)%.") do
+            table.insert(result, match)
+        end
+        while n > 0 do
+            table.remove(result)
+            n = n - 1
+        end
+    else
+        table.insert(result, str)
+    end
+    if type(sub) == "string" then
+        table.insert(result, sub)
+    end
+    return #result == 1 and result[1] or table.concat(result, ".")
+end
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.loader.enable()
