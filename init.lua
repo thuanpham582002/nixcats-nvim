040f932e8d6af2fa8410963e0b3279db8acff0d6
BirdeeHub <birdee@localhost>
2025-06-03 00:16:57 -0700

    Added an easter egg for when you type 'help'
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   init.lua
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   cats.nix
    	modified:   lua/testfnl.fnl
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    	pack/personalplugins/start/fn_finder

--- init.lua
+++ init.lua
@@ -32,7 +32,7 @@ require('nixCatsUtils').setup { non_nix_value = true }
 if vim.g.vscode == nil then
     if nixCats('fennel') then
         -- trivia: <c-k>*l is λ
-        require("fn_finder").fnl.install { uniquevar = nixCats.settings, search_opts = { nvim = true }, }
+        require("fn_finder").fnl.install { nixCats.packageBinPath, search_opts = { nvim = true }, }
     end
     require('birdee')
 end
