1c6999f0eee8581a727658998fada1d7ca2ed3e2
thuanpham582002 <tienthuan05082002@gmail.com>
2025-11-01 13:54:50 +0700

    fix: use exact claudecode pattern for obsidian

--- cats.nix
+++ cats.nix
@@ -340,15 +340,21 @@ in { pkgs, settings, categories, name, extra, mkPlugin, ... }@packageDef: {
       ];
     };
     obsidian = [
-    pkgs.vimUtils.buildVimPlugin {
-      pname = "obsidian-nvim";
-      version = "2025-01-01";
-      src = builtins.fetchTarball {
-        url = "https://github.com/obsidian-nvim/obsidian.nvim/archive/1a1a475846a4cfa3cfedde1c59141d99b6212951.tar.gz";
-        sha256 = "1b337e6220d57039d9eae9ec0eb0d104fcbf9946abe611861462d4a1bb9636cac";
-      };
-    }
-  ];
+      (pkgs.vimUtils.buildVimPlugin {
+        pname = "obsidian.nvim";
+        version = "2025-01-01";
+        src = pkgs.fetchFromGitHub {
+          owner = "obsidian-nvim";
+          repo = "obsidian.nvim";
+          rev = "1a1a475846a4cfa3cfedde1c59141d99b6212951";
+          hash = "sha256-b337e6220d57039d9eae9ec0eb0d104fcbf9946abe611861462d4a1bb9636cac";
+        };
+        meta = {
+          description = "Obsidian.md integration for Neovim";
+          homepage = "https://github.com/obsidian-nvim/obsidian.nvim";
+        };
+      })
+    ];
     debug = [
       nvim-dap
       nvim-dap-ui
